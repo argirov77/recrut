2384cff750a188be051b0309ec06177a5a687fb0
argirov77 <164051115+argirov77@users.noreply.github.com>
2025-07-08 13:09:18 +0200

    refactor: centralize admin sidebar

--- frontend/src/pages/JobAdmin.tsx
+++ frontend/src/pages/JobAdmin.tsx
@@ -1,18 +1,14 @@
 // pages/JobAdmin.tsx
 
-import AdminHeader    from '../components/admin/AdminHeader'
-import AdminSidebar   from '../components/admin/AdminSidebar'
-import AdminJobList   from '../components/admin/AdminJobList'
+import AdminHeader  from '../components/admin/AdminHeader'
+import AdminJobList from '../components/admin/AdminJobList'
 
 export default function JobAdmin() {
   return (
-    <div className="flex h-full">
-      <AdminSidebar />
-      <div className="flex-1">
-        <AdminHeader />
-        <div className="p-6">
-          <AdminJobList />
-        </div>
+    <div className="flex-1">
+      <AdminHeader />
+      <div className="p-6">
+        <AdminJobList />
       </div>
     </div>
   )
